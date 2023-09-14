8720df782e6ba2bb716822e38cc7f6b90658b43b
Dodul01 <mhdodul299@gmail.com>
2023-09-14 21:19:38 +0600

    JSON Data Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(()=>{
+    fetch('./data.json')
+    .then(response => response.json())
+    .then(result => setCourses(result))
+  },[])
+
   return (
     <div className='max-w-7xl mx-auto'>
+      {/* Nav Bar */}
       <nav className='m-2'>
         <h1 className='font-bold text-3xl text-center'>Course Registration</h1>
       </nav>
