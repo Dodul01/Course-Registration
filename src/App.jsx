993125da9ff90536db2533366ee4d28c0a4b4905
Dodul01 <mhdodul299@gmail.com>
2023-09-14 20:41:09 +0600

    Nav Bar added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='max-w-7xl mx-auto'>
+      <nav className='m-2'>
+        <h1 className='font-bold text-3xl text-center'>Course Registration</h1>
+      </nav>
+    </div>
+  )
+}
+
+
+export default App;
